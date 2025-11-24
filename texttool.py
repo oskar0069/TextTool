77e9a3e1ffeec6c28042f1a25658ac9222d85959
A <gosco6689@gmail.com>
2025-11-24 22:15:13 +0100

    Add length command to TextTool

--- texttool.py
+++ texttool.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 
-
-
 def process_line(line):
     if " " not in line:
         return "No command or no argument given"
 
     cmd, text = line.split(" ", maxsplit=1)
 
+    # Commandes existantes
     if cmd == "uppercase":
         return text.upper()
     if cmd == "lowercase":
         return text.lower()
 
-    return "Unknown command " + cmd
+    # 🌟 Commande ajoutée par l'utilisateur A (Exercice 3)
+    # length → renvoie la longueur de text
+    if cmd == "length":
+        return str(len(text))
 
+    return "Unknown command " + cmd
 
 
 def main():
     while True:
         try:
-            line = input("commade> ")
+            line = input("commande> ")
         except EOFError:
             break
 
         print(process_line(line))
-
 
 
 if __name__ == "__main__":
